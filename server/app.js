08fe90cdc6a65f7b8982e870d20bf26dbfd0f326
brokim <adslisdm@gmail.com>
2020-10-27 18:29:07 +0900

    secret information linked with dotenv and made session setting

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const session = require('express-session');
+const mysqlStore = require('express-mysql-session')(session);
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -7,9 +8,37 @@ const port = process.env.SERVER_PORT || 3355;
 const dotenv = require('dotenv');
 dotenv.config();
 
+// declare env variable
+const env = process.env;
+
+const options = {
+  username: env.username,
+  password: env.password,
+  database: env.database,
+  host: env.host,
+  port: env.port,
+  dialect: env.dialect
+}
+
+const sessionStorage = new mysqlStore(options);
+
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
+
+// mysql session managing
+app.use(
+  session({
+    secret: env.secret,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStorage,
+    cookie: {
+      domain: 'http://localhost:3355',
+      expires: new Date(Date.now() + (20000))
+    }
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('welcome to the travel help!');
---
+++ server/config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const env = process.env;
+
+const development = {
+  username: env.username,
+  password: env.password,
+  database: env.database,
+  host: env.host,
+  port: env.port,
+  dialect: env.dialect
+};
+
+const test = {
+  username: env.username,
+  password: env.password,
+  database: env.database,
+  host: env.host,
+  port: env.port,
+  dialect: env.dialect
+};
+
+const production = {
+  username: env.username,
+  password: env.password,
+  database: env.database,
+  host: env.host,
+  port: env.port,
+  dialect: env.dialect
+};
+
+module.exports = { development, test, production };
