d93c85f74b32bad7d6ce4df3d48cd37a0eb0d9f6
brokim <adslisdm@gmail.com>
2020-10-27 17:37:55 +0900

    initial index page setting

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const session = require('express-session');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const port = process.env.SERVER_PORT || 3355;
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('welcome to travel help!');
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on localhost:${port}`);
+});
+
+module.exports = app;
