569aa443bedf016b467132feeb9af74f5c66f5ea
brokim <adslisdm@gmail.com>
2020-10-29 18:09:47 +0900

    [server/update] removed unnecessary codes

--- server/config/config.js
+++ server/config/config.js
@@ -30,18 +30,3 @@ const production = {
 };
 
 module.exports = { development, test, production };
-
-// if (!env.host) {
-//   dotenv.config({
-//     path: path.join(__dirname, "..", ".env")
-//   })
-// }
-
-// module.exports = {
-//   username: env.username,
-//   password: env.password,
-//   database: env.database,
-//   host: env.host,
-//   port: env.port,
-//   dialect: env.dialect
-// }
