5fb8a0ab40a032846292ebb0bac38c1f16787147
brokim <adslisdm@gmail.com>
2020-10-30 18:05:55 +0900

    [server/update] added condition for signing out

--- server/controller/user/signout.js
+++ server/controller/user/signout.js
@@ -2,7 +2,7 @@ const { User } = require('../../models');
 const bcrypt = require('bcrypt');
 
 /*
-1. check req.session.user.id
+1. check req.session.user.id for checking user sign in status.
 2. update user's last visited & language field on db reffering user's session
 3. destroy user's session
 */
@@ -10,25 +10,33 @@ const bcrypt = require('bcrypt');
 module.exports = {
   post: async (req, res) => {
     
-    const { language } = req.body;
-    
-    console.log('User Language:', language);
-    console.log('req.session.user_id: ', req.session.user_id);
-    
-    // update last visited time & language setting of the user
-    await User.update({
-      last_visited_at: new Date(),
-      language: language
-    }, {  
-      where: {
-        id: req.session.user_id
-      }
-    })
-
-    await req.session.destroy((err) => {
-      req.session;
-      res.status(200).send("successfully signed out");
-    });
+    // check user has signed in. else case? do nothing.
+    if (req.session.user_id) {
+      const { language } = req.body;
+  
+      console.log('User Language:', language);
+      console.log('req.session.user_id: ', req.session.user_id);
+      
+      // update last visited time & language setting of the user
+      await User.update({
+        last_visited_at: new Date(),
+        language: language
+      }, {  
+        where: {
+          id: req.session.user_id
+        }
+      })
+  
+      // end sesssion. redirection will be done from the client side
+      await req.session.destroy((err) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          res.status(200).send("successfully signed out");
+        }
+      });
+    }
     
   }
 };
