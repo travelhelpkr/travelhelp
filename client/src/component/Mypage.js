7cda3720927dc30c25be31cd05a60d1b2d57f3db
chaerynny <ryn3777@gmail.com>
2020-11-01 17:48:01 +0900

    [client/add] mypage component

--- client/src/component/Mypage.js
+++ client/src/component/Mypage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import AlternateEmailOutlinedIcon from '@material-ui/icons/AlternateEmailOutlined';
 import '../scss/Mypage.scss';
 
 function Mypage(props) {
@@ -23,7 +25,27 @@ function Mypage(props) {
 
   return(
     <div className="background">
-      <button onClick={signoutHandler}>로그아웃</button>
+
+      <h1 className="h1">마이페이지</h1>
+
+      <div className="content_userInfo">
+
+        {/* user infomation */}
+        <div className="userInfo">
+          <span className="userInfoIcon"><AccountCircleOutlinedIcon /></span>
+          <span className="userInfoText">chae ryn park</span>
+        </div>
+        <div className="userInfo">
+          <span className="userInfoIcon"><AlternateEmailOutlinedIcon /></span>
+          <span className="userInfoText">ryn3777@gmail.com</span>
+        </div>
+        <button onClick={signoutHandler}>비밀번호 변경</button>
+        <button onClick={signoutHandler}>로그아웃</button>
+      </div>
+
+      <div className="content_orderInfo">
+        <div>주문내역</div>
+      </div>
     </div>
   )
 }
