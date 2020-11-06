63c3ec8a0013819583893af9815574ff04e25321
chaerynny <ryn3777@gmail.com>
2020-11-06 23:19:30 +0900

    [server/add] get restaurant information api

---
+++ server/controller/food/restaurant.js
@@ -0,0 +1,34 @@
+const { Restaurant } = require('../../models');
+
+/*
+1. check id of restaurant table
+2. send restaurant information matched with id(PK)
+*/
+
+module.exports = {
+  get: async (req, res) => {
+
+    // check restaurant id
+    const id = req.query.id;
+
+    // get restaurant information with id of restaurant table
+    await Restaurant.findOne({
+      where: {
+        id: id
+      }
+    })
+    .then(result => {
+      res.status(200).send({
+        "name_en": result.dataValues.name_en,
+        "name_zh": result.dataValues.name_zh,
+        "name_ja": result.dataValues.name_ja,
+        "description_en": result.dataValues.description_en,
+        "description_zh": result.dataValues.description_zh,
+        "description_ja": result.dataValues.description_ja,
+        "operation_hour": result.dataValues.operation_hour,
+        "minimum_price": result.dataValues.minimum_price,
+        "delivery_fee": result.dataValues.delivery_fee
+      });
+    })
+  }
+};
