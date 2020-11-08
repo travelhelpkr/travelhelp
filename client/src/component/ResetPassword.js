bdee68f9b9e089594ac5264f7036e2089e9ed891
chaerynny <ryn3777@gmail.com>
2020-11-08 14:34:56 +0900

    [client/update] reset password btn handler

--- client/src/component/ResetPassword.js
+++ client/src/component/ResetPassword.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import CloseIcon from '@material-ui/icons/Close';
 import "../scss/Modal.scss";
+import axios from 'axios';
 
 function ResestPassword(props) {
 
   // get from each menu
   const { isOpen, setModal } = props;
 
+  // input state of user information for signin
+  const [email, inputEmail] = useState('');
+
   // change language handler
   const { t } = useTranslation();
+
+  // reset password handler
+  const handleResetPasswordBtn = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3355/users/reset_password", {
+      email: email
+    },{
+      withCredentials: true,
+    }, {
+      headers: { 
+        "Access-Control-Allow-Origin": "http://localhost:3355",
+       }
+    })
+    .then(res => {
+      console.log("res:", res);
+      inputEmail("");
+    })
+  }
 
   return(
     <div className={isOpen ? "openModal" : "none"}>
@@ -26,9 +48,9 @@ function ResestPassword(props) {
           <div className="step1">{t('resetPassword.step1')}</div>
           <div className="emailAddress">{t('resetPassword.email')}</div>
           <form>
-            <input className="emailInput" type="text" name="email" placeholder={t("signin.email")} label="Email Address" />
+            <input className="emailInput" type="text" name="email" onChange={(e) => inputEmail(e.target.value)} placeholder={t("signin.email")} label="Email Address" />
           </form>
-          <button className="resetPasswordBtn">{t('resetPassword.btn')}</button>
+          <button className="resetPasswordBtn" onClick={handleResetPasswordBtn}>{t('resetPassword.btn')}</button>
         </div>
       </div>
     </div>
