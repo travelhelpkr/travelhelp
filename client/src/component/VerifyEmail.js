901e046e88b3be0620fca67f5fbce97a0fe3d40f
chaerynny <ryn3777@gmail.com>
2020-11-10 23:56:04 +0900

    [client/update] redirect after reset password

---
+++ client/src/component/VerifyEmail.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { withRouter } from "react-router-dom";
+import axios from 'axios';
+
+function VerifyEmail() {
+
+  // get url
+  const url = new URL(window.location.href);
+
+  useEffect(() => {
+    axios.get('http://localhost:3355/auth/email', {
+      params: {
+        token: url.searchParams.get('token') 
+      }
+    })
+    .then(() => window.location = "/user/signin");
+  },[])
+
+  return(
+    <div>
+
+    </div>
+  )
+
+}
+
+export default withRouter(VerifyEmail); 
