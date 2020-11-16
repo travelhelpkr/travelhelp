6be33d068da0136042037c56c455d3e030db3f8f
chaerynny <ryn3777@gmail.com>
2020-11-16 17:12:06 +0900

    [client/update] make alert depends on response status code

--- client/src/component/ModalChicken.js
+++ client/src/component/ModalChicken.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import CloseIcon from '@material-ui/icons/Close';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
 import '../scss/Modal.scss';
 
 function ModalChicken(props) {
@@ -18,27 +19,42 @@ function ModalChicken(props) {
   // alert after add to cart btn
   const [success, setSuccess] = useState(false);
   const [failure, setFailure] = useState(false);
+  const [optionError, setOptionError] = useState(false);
 
+  // add to cart btn handler
   const addToCartHandler = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3355/foods/cart', {
-      user_id: window.sessionStorage.getItem('id'),
-      menu_id: infoMenuId,
-      option_id: type
-    }, {
-      withCredentials: true,
-    }, {
-      headers: { 
-        'Access-Control-Allow-Origin': 'http://localhost:3355',
-       }
-    })
-    .then(res => {
-      if(res.data.status === 409) {
-        setFailure(true);
-      } else if(res.data.status === 200){
-        setSuccess(true);
-      }
-    })
+    if(type) {
+      axios.post('http://localhost:3355/foods/cart', {
+        user_id: window.sessionStorage.getItem('id'),
+        menu_id: infoMenuId,
+        option_id: type
+      }, {
+        withCredentials: true,
+      }, {
+        headers: { 
+          'Access-Control-Allow-Origin': 'http://localhost:3355',
+         }
+      })
+      .then(res => {
+        if(res.data.status === 409) {
+          setFailure(true);
+          setSuccess(false);
+          setOptionError(false);
+          setType('');
+        } else if(res.data.status === 200){
+          setSuccess(true);
+          setFailure(false);
+          setOptionError(false);
+          setType('');
+        }
+      })
+    } else {
+      setOptionError(true);
+      setSuccess(false);
+      setFailure(false);
+      setType('');
+    }
   }
 
   return(
@@ -49,6 +65,10 @@ function ModalChicken(props) {
         <button className='modalCloseBtn' onClick={e => {
           e.preventDefault();
           setModal(!isOpen);
+          setSuccess(false);
+          setFailure(false);
+          setOptionError(false);
+          setType('');
         }}><CloseIcon /></button>
 
         {/* menu information */}
@@ -68,23 +88,29 @@ function ModalChicken(props) {
         {/* choose bone or boneless */}
         <div className='selectTitle'>{infoOptionName1}/{infoOptionName2} *</div>
         <select className='selectBox' onChange={e => setType(e.target.value)}>
-          <option value='' disabled defaultValue>{infoOptionName1}/{infoOptionName2} ({t('modalChicken.required')})</option>
+          <option value=''>{infoOptionName1}/{infoOptionName2} ({t('modalChicken.required')})</option>
           <option value='2'>{infoOptionName1}</option>
           <option value='3'>{infoOptionName2} (+{infoOptionPrice2}₩)</option>
         </select>
+
+        {/* alert after clicking btn */}
+        <div className={optionError ? 'optionErrorAlert' : 'none'}>
+          <span>{t('modalCart.option')}</span>
+        </div>
 
         {/* add to cart btn */}
         <button className='addCartBtn' onClick={addToCartHandler}>{t('modalChicken.addToCart')}</button>
 
         {/* alert after clicking btn */}
         <div className={success ? 'successAlert' : 'none'}>
-          <span>Successfully added to the cart</span>
-          {/* <span className='signupLink'><a href='/user/signup'>{t('signup.signup')}</a></span> */}
+          <span><CheckCircleRoundedIcon />{t('modalCart.addToCart')}</span>
+          <span className='goToCart'><a href='/user/cart'>{t('modalCart.goToCart')}</a></span>
         </div>
         <div className={failure ? 'failureAlert' : 'none'}>
-          <span>The menu you chose are already in the cart.</span>
-          {/* <span className='signupLink'><a href='/user/signup'>{t('signup.signup')}</a></span> */}
+          <span>{t('modalCart.already')}</span>
+          <span className='goToCart'><a href='/user/cart'>{t('modalCart.goToCart')}</a></span>
         </div>
+
       </div>
     </div>
   )
