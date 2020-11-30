6775649d696ddd6fe406587fdc11afab51716312
brokim <adslisdm@gmail.com>
2020-12-01 00:33:47 +0900

    [deploy/update] configuration checking

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const session = require('express-session');
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const mysqlStore = require('express-mysql-session')(session);
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -9,8 +9,8 @@ const userRouter = require('./router/userRouter');
 const authRouter = require('./router/authRouter');
 const foodRouter = require('./router/foodRouter');
 const passport = require('passport');
-// const env = process.env.NODE_ENV || 'development';
-// const config = require('./config/config.js')[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config/config.js')[env];
 const port = process.env.SERVER_PORT || 8080;
 const dotenv = require('dotenv');
 dotenv.config();
@@ -24,21 +24,20 @@ app.use(cors({
 }));
 
 // declare env variable for managing session
-const env = process.env;
 const options = {
-  host: env.DB_HOST,
-  port: env.DB_PORT,
-  user: env.DB_USER_NAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_DATABASE,
-  // dialect: mysql
+  host: config.host,
+  port: config.port,
+  user: config.username,
+  password: config.password,
+  database: config.database,
+  // dialect: config.dialect
 }
 const sessionStorage = new mysqlStore(options);
 
 // mysql session managing
 app.use(
   session({
-    secret: env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: sessionStorage,
