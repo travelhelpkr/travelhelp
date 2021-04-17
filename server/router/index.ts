821af3d3deecefefb52e323bdfede62bc9669387
BroKim <18327369+imbrok@users.noreply.github.com>
2021-04-17 15:04:42 +0900

    thr #119 added index.ts for router folder

---
+++ server/router/index.ts
@@ -0,0 +1,9 @@
+import userRouter from './userRouter';
+import foodRouter from './foodRouter';
+import authRouter from './authRouter';
+
+export const Router = {
+  userRouter,
+  foodRouter,
+  authRouter
+}
