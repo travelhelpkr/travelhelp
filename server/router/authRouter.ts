8c68b92eab76503950a97e3b8a24e25edab15b8d
BroKim <18327369+imbrok@users.noreply.github.com>
2021-04-17 14:37:08 +0900

    thr #113 changed authRouter.js into .ts

---
+++ server/router/authRouter.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import passport from 'passport';
+
+import { Auth } from '../controller/auth';
+
+const passportLine = require('../controller/auth/passportLine');
+
+const env: string = process.env.NODE_ENV || 'production';
+const config = require(__dirname + '/../config/config.js')[env];
+
+const authRouter = express.Router();
+
+// local
+authRouter.post('/email', Auth.resendEmail);
+authRouter.get('/email', Auth.updateEmail);
+authRouter.post('/resetPassword', Auth.sendEmail);
+authRouter.get('/password', Auth.verifyToken);
+authRouter.post('/password', Auth.updatePassword);
+
+// google
+Auth.google();
+authRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+authRouter.get('/google/callback', passport.authenticate('google', { failureRedirect: `${config.client_url}/user/signin` })
+, (req: Request, res: Response) => {
+  // defined variables will be tossed to the cookie of the browser
+  const { id, name, email, oauth_provider, language }: { id:number, name:string, email:string, oauth_provider:string, language:string } = req.session.passport.user;
+  res.cookie('user', { id, name, email, oauth_provider, language });
+  res.redirect(`${config.client_url}`);
+} );  
+
+// line
+passportLine();
+authRouter.get('/line', passport.authenticate('line'));
+authRouter.get('/line/callback', passport.authenticate('line', { failureRedirect: `${config.client_url}/user/signin` })
+, (req: Request, res: Response) => {
+  // defined variables will be tossed to the cookie of the browser
+  const { id, name, email, oauth_provider, language }: { id:number, name:string, email:string, oauth_provider:string, language:string } = req.session.passport.user;
+  res.cookie('user', { id, name, email, oauth_provider, language });
+  res.redirect(`${config.client_url}`);
+} );
+
+module.exports = authRouter;
