f22afd58601cc2d365b4c9c85ad47cd0f345687d
BroKim <18327369+imbrok@users.noreply.github.com>
2021-04-18 22:55:40 +0900

    optimized app.ts file

--- server/app.ts
+++ server/app.ts
@@ -1,21 +1,47 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
-import * as expressSession from 'express-session';
+import * as session from 'express-session';
 import expressMysqlSession from 'express-mysql-session';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import passport from 'passport';
 
-import { Router } from './router';
+import { routes } from './routes';
 
 dotenv.config();
 const app: express.Application = express();
 
+// server environments
 const env: string = process.env.NODE_ENV || 'production';
-const config: any = require('./config/config.js')[env];
-const port: string | number = process.env.SERVER_PORT || 8080;
-const MySQLStore: any = expressMysqlSession(expressSession);
+const config = require('./config/config.js')[env];
+
+
+
+/************************************************************************************
+ *                       Express session setting with MySQL
+ ***********************************************************************************/
+
+// configurations for session storage && connection pool on mysql
+const options = {
+  host: config.host,
+  port: config.port,
+  user: config.username,
+  password: config.password,
+  database: config.database,
+  // number of connections when creating a connection pool
+  connectionLimit: 10
+}
+// connecting an existing mysql pool(created by sequelize)
+const MySQLStore = expressMysqlSession(session);
+const connection: object = mysql.createPool(options);
+const sessionStorage = new MySQLStore(options, connection);
+
+
+
+/************************************************************************************
+ *                       Set Express server app middlewares
+ ***********************************************************************************/
 
 app.use(cookieParser());
 app.use(express.json());
@@ -25,24 +51,9 @@ app.use(cors({
   credentials: true
 }));
 
-// configurations for session storage && connection pool on mysql
-const options: any = {
-  host: config.host,
-  port: config.port,
-  user: config.username,
-  password: config.password,
-  database: config.database,
-  // number of connections when creating a connection pool
-  connectionLimit: 10
-}
-
-// connecting an existing mysql pool(created by sequelize)
-const connection: object = mysql.createPool(options);
-const sessionStorage = new MySQLStore(options, connection);
-
 // mysql session managing
 app.use(
-  expressSession.default({
+  session.default({
     secret: config.sessionSecret,
     resave: false,
     saveUninitialized: false,
@@ -58,20 +69,20 @@ app.use(
   })
 );
   
-  // passport
+// passport
 app.use(passport.initialize());
 app.use(passport.session())
 
-// routing
-app.use('/api/users', Router.userRouter);
-app.use('/api/auth', Router.authRouter);
-app.use('/api/foods', Router.foodRouter);
+// set API routing
+app.use('/', routes);
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  //  console.log('sessison: ', req.session);
-  //  console.log('cookies: ', req.cookies);
-  res.send('welcome to the travel help!');
-})
+
+
+/************************************************************************************
+ *                       Start the Express server app
+ ***********************************************************************************/
+
+const port: number = <unknown>process.env.SERVER_PORT as number || 8080;
 
 app.listen(port, () => {
   console.log(`Server listening on port:${port}`);
